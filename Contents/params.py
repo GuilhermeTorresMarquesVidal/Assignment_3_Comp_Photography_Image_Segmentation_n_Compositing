fd5b926147045f96743ee4b00f4a6bfe186bf882
GuilhermeTorresMarquesVidal <guilherme_torres@hotmail.com.br>
2021-09-16 02:28:25 -0300

    Gauss and Laplace pyramid begin

--- Contents/params.py
+++ Contents/params.py
@@ -5,18 +5,39 @@ import numpy as np
 limit_up = 255
 limit_down = 0
 
+# Sigma value
+
+sigma = 5
+
+# Zoom value
+
+zoom_in = 0.5
+zoom_out = 2.0
+
 # Keys
+
+keys_II = [
+    'apple',
+    'orange'
+]
 
 keys_III = [
     'background', 
     'GT04_alpha', 
     'GT04', 
-    'selfie']
+    'selfie'
+]
 
 # list of images filepath
+
+list_of_files_II = [
+    'Images/Apple.png',
+    'Images/Orange.png'
+]
 
 list_of_files_III = [
     'Images/background.png', 
     'Images/GT04_alpha.png', 
     'Images/GT04.png', 
-    'Images/selfie.jpg']
+    'Images/selfie.jpg'
+]
